3d1166e45189143dc43e046b7f6741ded424ee42
Chakron Yuraket <72598195+Assfirst@users.noreply.github.com>
2025-03-17 12:59:18 +0700

    app.py

--- my_it_store/app.py
+++ my_it_store/app.py
@@ -242,5 +242,5 @@ with app.app_context():
     else:
         print(f"Using existing database at {db_path}")
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
